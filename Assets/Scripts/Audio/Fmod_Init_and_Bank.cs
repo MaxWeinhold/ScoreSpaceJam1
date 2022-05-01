50c5719310cec886418df9e19b469f586740ceac
Alios <80349968+Alios-5@users.noreply.github.com>
2022-05-01 23:02:54 +0300

    added bank + music player script + webgl audio init

---
+++ Assets/Scripts/Audio/Fmod_Init_and_Bank.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fmod_Init_and_Bank : MonoBehaviour
+{
+
+    bool audioResumed = false;
+    void Awake()
+    {
+        if (!audioResumed)
+        {
+            var result = FMODUnity.RuntimeManager.CoreSystem.mixerSuspend();
+            //  Debug.Log(result);
+            result = FMODUnity.RuntimeManager.CoreSystem.mixerResume();
+            //  Debug.Log(result);
+            audioResumed = true;
+        }
+
+        FMODUnity.RuntimeManager.LoadBank("Main");
+        FMODUnity.RuntimeManager.LoadBank("Main.strings");
+
+    }
+
+
+}
---
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public static MusicPlayer instance;
+
+    private FMOD.Studio.EventInstance MainMusic;
+
+
+
+    void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        MainMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Main_Music");
+    }
+
+
+
+    public void PlayMainMusic()
+    {
+        MainMusic.start();
+    }
+    public void StopMainMusic()
+    {
+        MainMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+}
